17a53a7b37ee71874f2075cd06232d0fe6fbcec9
QMEOWQ <3188986027@qq.com>
2026-04-15 14:15:24 +0800

    add dlx dlq

--- internal/common/broker/rabbitmq.go
+++ internal/common/broker/rabbitmq.go
@@ -9,7 +9,19 @@ import (
 	"go.opentelemetry.io/otel"
 )
 
+const (
+	DLX = "dlx"
+	DLQ = "dlq"
+)
+
 func Connect(user, password, host, port string) (*amqp.Channel, func() error) {
+	// var err error
+	// go func() {
+	// 	if err != nil {
+	// 		logrus.Fatal(err)
+	// 	}
+	// }()
+
 	address := fmt.Sprintf("amqp://%s:%s@%s:%s", user, password, host, port)
 	conn, err := amqp.Dial(address)
 	if err != nil {
@@ -27,7 +39,28 @@ func Connect(user, password, host, port string) (*amqp.Channel, func() error) {
 	if err != nil {
 		logrus.Fatal(err)
 	}
+	if err = createDLX(ch); err != nil {
+		logrus.Fatal(err)
+	}
+
 	return ch, conn.Close
+}
+
+func createDLX(ch *amqp.Channel) error {
+	q, err := ch.QueueDeclare("share_queue", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	err = ch.ExchangeDeclare(DLX, "fanout", true, false, false, false, nil)
+	if err != nil {
+		return err
+	}
+	err = ch.QueueBind(q.Name, "", DLX, false, nil)
+	if err != nil {
+		return err
+	}
+	_, err = ch.QueueDeclare(DLQ, true, false, false, false, nil)
+	return err
 }
 
 type RabbitMQHeaderCarrier map[string]interface{}
--- internal/order/http.go
+++ internal/order/http.go
@@ -9,6 +9,7 @@ import (
 	"github.com/QMEOWQ/go-microservice-proj/common/tracing"
 	"github.com/QMEOWQ/go-microservice-proj/order/app"
 	"github.com/QMEOWQ/go-microservice-proj/order/app/command"
+	"github.com/QMEOWQ/go-microservice-proj/order/app/dto"
 	"github.com/QMEOWQ/go-microservice-proj/order/app/query"
 	"github.com/QMEOWQ/go-microservice-proj/order/convertor"
 	"github.com/gin-gonic/gin"
@@ -21,26 +22,39 @@ type HTTPServer struct {
 
 // GetCustomerCustomerIdOrdersOrderId implements [ports.ServerInterface].
 func (H HTTPServer) GetCustomerCustomerIdOrdersOrderId(c *gin.Context, customerId string, orderId string) {
-	panic("unimplemented")
+	_, span := tracing.Start(c, "GetCustomerCustomerIDOrdersOrderID")
+	defer span.End()
+
+	var (
+		err  error
+		resp interface{}
+	)
+	defer func() {
+		H.Response(c, err, resp)
+	}()
+
+	o, err := H.app.Queries.GetCustomerOrder.Handle(c, query.GetCustomerOrder{
+		// OrderID:    "fake-ID",
+		// CustomerID: "fake-customer-id",
+		OrderID:    orderId,
+		CustomerID: customerId,
+	})
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{"error": err})
+		return
+	}
+	resp = convertor.NewOrderConvertor().EntityToClient(o)
 }
 
 // PostCustomerCustomerIdOrders implements [ports.ServerInterface].
 func (H HTTPServer) PostCustomerCustomerIdOrders(c *gin.Context, customerId string) {
-	panic("unimplemented")
-}
-
-func (H HTTPServer) PostCustomerCustomerIDOrders(c *gin.Context, customerID string) {
 	_, span := tracing.Start(c, "PostCustomerCustomerIDOrders")
 	defer span.End()
 
 	var (
 		req  client.CreateOrderRequest
 		err  error
-		resp struct {
-			CustomerID  string `json:"customer_id"`
-			OrderID     string `json:"order_id"`
-			RedirectURL string `json:"redirect_url"`
-		}
+		resp dto.CreateOrderResponse
 	)
 	defer func() {
 		H.Response(c, err, &resp)
@@ -56,32 +70,9 @@ func (H HTTPServer) PostCustomerCustomerIDOrders(c *gin.Context, customerID stri
 	if err != nil {
 		return
 	}
-	resp.CustomerID = req.CustomerId
-	resp.RedirectURL = fmt.Sprintf("http://localhost:8282/success?customerID=%s&orderID=%s", req.CustomerId, r.OrderID)
-	resp.OrderID = r.OrderID
-}
-
-func (H HTTPServer) GetCustomerCustomerIDOrdersOrderID(c *gin.Context, customerID string, orderID string) {
-	_, span := tracing.Start(c, "GetCustomerCustomerIDOrdersOrderID")
-	defer span.End()
-
-	var (
-		err  error
-		resp interface{}
-	)
-	defer func() {
-		H.Response(c, err, resp)
-	}()
-
-	o, err := H.app.Queries.GetCustomerOrder.Handle(c, query.GetCustomerOrder{
-		// OrderID:    "fake-ID",
-		// CustomerID: "fake-customer-id",
-		OrderID:    orderID,
-		CustomerID: customerID,
-	})
-	if err != nil {
-		c.JSON(http.StatusOK, gin.H{"error": err})
-		return
+	resp = dto.CreateOrderResponse{
+		OrderID:     r.OrderID,
+		CustomerID:  req.CustomerId,
+		RedirectURL: fmt.Sprintf("http://localhost:8282/success?customerID=%s&orderID=%s", req.CustomerId, r.OrderID),
 	}
-	resp = convertor.NewOrderConvertor().EntityToClient(o)
 }
